b031abc271d94dcc2d2a4cd5fa0c612465ecb3cd
Junbum Lee <jun@beomi.net>
2024-04-17 23:26:48 +0900

    Add train.gemma.infini.noclm

---
+++ train.gemma.infini.noclm.sh
@@ -0,0 +1,12 @@
+python train.gemma.infini.noclm.py \
+    --model_name_or_path='google/gemma-2b' \
+    --segment_length=2048 \
+    --block_size=32768 \
+    --dataset_name='wikitext' \
+    --dataset_config_name='wikitext-2-raw-v1' \
+    --per_device_train_batch_size=1 \
+    --output_dir='./models/gemma-2b-infini-noclm-wikitext' \
+    --num_train_epochs=1 \
+    --learning_rate=5e-5 \
+    --seed=42 \
+    --low_cpu_mem_usage
